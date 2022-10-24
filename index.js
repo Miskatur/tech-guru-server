a7e8cfd85fab475aefbf742e962394585890d783
Miskatur <Miskaturrahman34826@gmail.com>
2022-10-25 02:15:39 +0600

    index js completed and vercel added

--- index.js
+++ index.js
@@ -4,9 +4,30 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 app.use(cors())
 
+const courses = require('./data/courses.json');
+const courseDetails = require('./data/courses-details.json');
 
 app.get('/', (req, res) => {
     res.send('Educational Courses API running')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(c => c.id == id);
+    res.send(course)
+})
+
+app.get('/course-details', (req, res) => {
+    res.send(courseDetails)
+})
+
+app.get('/course-detail/:id', (req, res) => {
+    const id = req.params.id;
+    const courseDetail = courseDetails.find(c => c.id == id);
+    res.send(courseDetail)
 })
 
 
